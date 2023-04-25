931d161042316ded9d4eea0b93b82709646853f6
Tanvir_Ejaj <tanvirejajtushar@gmail.com>
2023-04-25 22:43:42 +0600

    block-complete

--- src/Components/BlockedUser/index.js
+++ src/Components/BlockedUser/index.js
@@ -1,16 +1,93 @@
-import React from 'react'
-import './style.css'
+import React, { useEffect, useState } from "react";
+import "./style.css";
+import Button from "@mui/material/Button";
+import {
+  getDatabase,
+  onValue,
+  push,
+  ref,
+  remove,
+  set,
+} from "firebase/database";
+import { useSelector } from "react-redux";
 
 const BlockedUser = () => {
+  const db = getDatabase();
+  const user = useSelector((users) => users.loginSlice.login);
+  const [blockUser, setBlockUser] = useState([]);
+
+  // Block list show
+
+  useEffect(() => {
+    const starCountRef = ref(db, "block");
+    onValue(starCountRef, (snapshot) => {
+      let blockArray = [];
+      snapshot.forEach((item) => {
+        if (item.val().blockedbyid === user.uid) {
+          blockArray.push({
+            id: item.key,
+            block: item.val().block,
+            blockedid: item.val().blockedid,
+          });
+        } else {
+          blockArray.push({
+            id: item.key,
+            blockedby: item.val().blockedby,
+            blockedbyid: item.val().blockedbyid,
+          });
+        }
+      });
+      setBlockUser(blockArray);
+    });
+  }, []);
+
+  // Unblock Friend
+
+  const handleUnblock = (item) => {
+    set(push(ref(db, "friends")), {
+      sendername: item.block,
+      senderid: item.blockedid,
+      receiverid: user.uid,
+      receivername: user.displayName,
+    }).then(() => {
+      remove(ref(db, "block/" + item.id));
+    });
+  };
+
   return (
     <div>
-        <div className="block-list-main">
-            <div className="header">
-                <h3>Blocked Users</h3>
-            </div>
+      <div className="block-main">
+        <div className="header">
+          <h3>Blocked Users</h3>
         </div>
+        <div className="block-body">
+          {blockUser.map((item, i) => (
+            <div className="block-inner" key={i}>
+              <div className="block-pics">
+                <picture>
+                  <img src="./images/profile-pic.png" alt="friend-pic" />
+                </picture>
+              </div>
+              <div className="block-text">
+                <h3>{item.block}</h3>
+                <h3>{item.blockedby}</h3>
+              </div>
+              <div className="block-btn">
+                {!item.blockedbyid && (
+                  <Button
+                    variant="contained"
+                    onClick={() => handleUnblock(item)}
+                  >
+                    Unblock
+                  </Button>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlockedUser
+export default BlockedUser;
--- src/Components/Friends/index.js
+++ src/Components/Friends/index.js
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./style.css";
 import Button from "@mui/material/Button";
-import { getDatabase, onValue, ref } from "firebase/database";
+import {
+  getDatabase,
+  onValue,
+  push,
+  ref,
+  remove,
+  set,
+} from "firebase/database";
 import { useSelector } from "react-redux";
 
 const Friends = () => {
@@ -10,16 +17,42 @@ const Friends = () => {
   const user = useSelector((users) => users.loginSlice.login);
 
   // Show in friend list
+
   useEffect(() => {
     const starCountRef = ref(db, "friends");
     onValue(starCountRef, (snapshot) => {
       let friendArray = [];
       snapshot.forEach((item) => {
-        friendArray.push(item.val());
+        friendArray.push({ ...item.val(), id: item.key });
       });
       setFriendlist(friendArray);
     });
   }, []);
+
+  // Block Friend
+
+  const handleBlock = (item) => {
+    if (user.uid === item.senderid) {
+      set(push(ref(db, "block")), {
+        block: item.receivername,
+        blockedid: item.receiverid,
+        blockedby: item.sendername,
+        blockedbyid: item.senderid,
+      }).then(() => {
+        remove(ref(db, "friends/" + item.id));
+      });
+    } else {
+      set(push(ref(db, "block")), {
+        block: item.sendername,
+        blockedid: item.senderid,
+        blockedby: item.receivername,
+        blockedbyid: item.receiverid,
+      }).then(() => {
+        remove(ref(db, "friends/" + item.id));
+      });
+    }
+  };
+
   return (
     <>
       <div className="friends-main">
@@ -42,7 +75,9 @@ const Friends = () => {
                 </h3>
               </div>
               <div className="friends-btn">
-                <Button variant="contained">Block</Button>
+                <Button variant="contained" onClick={() => handleBlock(item)}>
+                  Block
+                </Button>
                 <Button variant="contained">Unfriend</Button>
               </div>
             </div>
